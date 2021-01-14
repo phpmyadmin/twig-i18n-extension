a701ab8941cefcb5d1361e719c8152c9828ff9a4
William Desportes <williamdes@wdes.fr>
2021-01-14 12:39:26 +0100

    Adjust tests after the signature for TransNode has changed

--- test/Node/TransTest.php
+++ test/Node/TransTest.php
@@ -37,7 +37,7 @@ class TransTest extends NodeTestCase
             new PrintNode(new NameExpression('count', 0), 0),
             new TextNode(' apples', 0),
         ], [], 0);
-        $node = new TransNode($body, $plural, $count, null, 0);
+        $node = new TransNode($body, $plural, $count, null, null, 0);
 
         $this->assertEquals($body, $node->getNode('body'));
         $this->assertEquals($count, $node->getNode('count'));
@@ -52,17 +52,17 @@ class TransTest extends NodeTestCase
         $tests = [];
 
         $body = new NameExpression('foo', 0);
-        $node = new TransNode($body, null, null, null, 0);
+        $node = new TransNode($body, null, null, null, null, 0);
         $tests[] = [$node, sprintf('echo gettext(%s);', $this->getVariableGetter('foo'))];
 
         $body = new ConstantExpression('Hello', 0);
-        $node = new TransNode($body, null, null, null, 0);
+        $node = new TransNode($body, null, null, null, null, 0);
         $tests[] = [$node, 'echo gettext("Hello");'];
 
         $body = new Node([
             new TextNode('Hello', 0),
         ], [], 0);
-        $node = new TransNode($body, null, null, null, 0);
+        $node = new TransNode($body, null, null, null, null, 0);
         $tests[] = [$node, 'echo gettext("Hello");'];
 
         $body = new Node([
@@ -70,7 +70,7 @@ class TransTest extends NodeTestCase
             new PrintNode(new NameExpression('foo', 0), 0),
             new TextNode(' pommes', 0),
         ], [], 0);
-        $node = new TransNode($body, null, null, null, 0);
+        $node = new TransNode($body, null, null, null, null, 0);
         $tests[] = [$node, sprintf('echo strtr(gettext("J\'ai %%foo%% pommes"), array("%%foo%%" => %s, ));', $this->getVariableGetter('foo'))];
 
         $count = new ConstantExpression(12, 0);
@@ -86,7 +86,7 @@ class TransTest extends NodeTestCase
             new PrintNode(new NameExpression('count', 0), 0),
             new TextNode(' apples', 0),
         ], [], 0);
-        $node = new TransNode($body, $plural, $count, null, 0);
+        $node = new TransNode($body, $plural, $count, null, null, 0);
         $tests[] = [$node, sprintf('echo strtr(ngettext("Hey %%name%%, I have one apple", "Hey %%name%%, I have %%count%% apples", abs(12)), array("%%name%%" => %s, "%%name%%" => %s, "%%count%%" => abs(12), ));', $this->getVariableGetter('name'), $this->getVariableGetter('name'))];
 
         // with escaper extension set to on
@@ -96,18 +96,18 @@ class TransTest extends NodeTestCase
             new TextNode(' pommes', 0),
         ], [], 0);
 
-        $node = new TransNode($body, null, null, null, 0);
+        $node = new TransNode($body, null, null, null, null, 0);
         $tests[] = [$node, sprintf('echo strtr(gettext("J\'ai %%foo%% pommes"), array("%%foo%%" => %s, ));', $this->getVariableGetter('foo'))];
 
         // with notes
         $body = new ConstantExpression('Hello', 0);
         $notes = new TextNode('Notes for translators', 0);
-        $node = new TransNode($body, null, null, $notes, 0);
+        $node = new TransNode($body, null, null, $notes, null, 0);
         $tests[] = [$node, "// notes: Notes for translators\necho gettext(\"Hello\");"];
 
         $body = new ConstantExpression('Hello', 0);
         $notes = new TextNode("Notes for translators\nand line breaks", 0);
-        $node = new TransNode($body, null, null, $notes, 0);
+        $node = new TransNode($body, null, null, $notes, null, 0);
         $tests[] = [$node, "// notes: Notes for translators and line breaks\necho gettext(\"Hello\");"];
 
         $count = new ConstantExpression(5, 0);
@@ -118,7 +118,7 @@ class TransTest extends NodeTestCase
             new TextNode(' pending tasks', 0),
         ], [], 0);
         $notes = new TextNode('Notes for translators', 0);
-        $node = new TransNode($body, $plural, $count, $notes, 0);
+        $node = new TransNode($body, $plural, $count, $notes, null, 0);
         $tests[] = [$node, "// notes: Notes for translators\n" . 'echo strtr(ngettext("There is 1 pending task", "There are %count% pending tasks", abs(5)), array("%count%" => abs(5), ));'];
 
         return $tests;
